e585f7cdce80f189f3a2de7c318aaff56b896e15
KurniaYufi <162095868+KurniaYufi@users.noreply.github.com>
2024-12-03 18:39:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.nav-links a, .nav-links2 a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
